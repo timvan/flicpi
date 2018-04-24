749fe2fb8cc33beb00a2effe62a587e47822778b
timvan <timvanellemeet@gmail.com>
2018-04-24 14:42:25 +1000

    test click_type

--- python/flicpi.py
+++ python/flicpi.py
@@ -7,14 +7,18 @@
 # It also monitors when new buttons are verified and connects to them as well. For example, run this program and at the same time the scan_wizard.py program.
 
 import fliclib
+import sqlite3
 
 client = fliclib.FlicClient("localhost")
+db = sqlite3.connect('flicpi.db')
 
 def got_button(bd_addr):
 	cc = fliclib.ButtonConnectionChannel(bd_addr)
-	cc.on_button_up_or_down = \
+	cc.on_button_single_or_double_click_or_hold = \
 		lambda channel, click_type, was_queued, time_diff: \
 			print(channel.bd_addr + " " + str(click_type))
+
+
 	cc.on_connection_status_changed = \
 		lambda channel, connection_status, disconnect_reason: \
 			print(channel.bd_addr + " " + str(connection_status) + (" " + str(disconnect_reason) if connection_status == fliclib.ConnectionStatus.Disconnected else ""))
