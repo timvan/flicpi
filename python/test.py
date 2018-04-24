eadf21f8f284bc99c2ce34dec90da7139780f80d
timvan <timvanellemeet@gmail.com>
2018-04-24 15:22:44 +1000

    get_status 2

---
+++ python/test.py
@@ -0,0 +1,13 @@
+import sqlite3
+db = sqlite3.connect('flicpi.db')
+print(db)
+bdAddr = ("80:80:80:80",)
+
+# c = db.cursor()
+
+# c.execute("SELECT * FROM event_log WHERE bdAddr=?", bdAddr)
+
+for row in db.execute("SELECT * FROM event_log WHERE bdAddr=? ORDER BY timestamp DESC LIMIT 1", bdAddr):
+	print("row:", row)
+
+print("[get_status]:")
