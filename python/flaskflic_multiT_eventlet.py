35a4d7b26bbd9a7a8e797d4aedb4a9ea38ba0da3
timvan <timvanellemeet@gmail.com>
2018-05-01 17:50:56 +1000

    bug

--- python/flaskflic_multiT_eventlet.py
+++ python/flaskflic_multiT_eventlet.py
@@ -114,7 +114,7 @@ def get_connected_devices():
 	socketio.emit('got connected devices', table)
 
 
-@socketio.on('scan wizard insert', new_user)
+@socketio.on('scan wizard insert')
 def scan_wizard_succes(new_user):
 
 
