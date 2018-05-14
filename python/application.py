79d78858e513ea135a5387228654a0bedc93e0bb
timvan <timvanellemeet@gmail.com>
2018-05-14 14:45:21 +1000

    import bug

--- python/application.py
+++ python/application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_socketio import SocketIO
 from datetime import date, datetime, timedelta
 import dateutil.parser
